dc7568591271a06d56a42d1cf22abc080452dffc
Talvarez <seba.alvarez16@gmail.com>
2014-03-01 04:57:51 -0300

    Se cambia eleccion de uerto

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var rosca = require(__dirname+'/routes/rosca');
 
+var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.configure(function() {
+	app.set('port', port);
+});
+
 app.get('/rosca/:partido', rosca.partido);
 
-var port = 3000;
 app.listen(port, function() {
 	console.log('Rosca iniciada en '+port);
 });
