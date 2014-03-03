0208df260ab0a03fde29827729cdc1b18629dd74
Talvarez <seba.alvarez16@gmail.com>
2014-03-03 02:52:10 -0300

    Se crea script que pregunta al servidor por los intendentes firmantes, para ver si siguen leales, se corrige nombre de un intendente en el archivo de firmantes

---
+++ res/chequear_leales.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+#Pregunta al servidor por los intendentes firmantes del acuerdo en Santa Teresita.
+
+use strict;
+
+open JSON_FILE, "< intendentes_firmantes_santa_teresita.txt";
+while(<JSON_FILE>) {
+	my $linea = $_;
+
+	my ($partido, $intendente) = split(":", $linea);
+	$partido =~ s/^\s+|\s+$//g;
+	$intendente =~ s/^\s+|\s+$//g;
+
+	print "---FIRMANTE---\n";
+	print "Partido: $partido\n";
+	print "Firmante: $intendente\n";
+
+	$partido =~ s/ /_/g;
+	my $cmd = "curl -s -X GET 'http://localhost:3000/rosca/$partido'";
+	my $output = `$cmd`;
+
+	if(!$output) {
+		print "Sin respuesta por parte del servidor\n";
+		print "DEBUG: $cmd\n";
+		exit -1;
+	}
+	my @arr_response = split("\n", $output);
+	print "\n---RESPUESTA---\n";
+	foreach(@arr_response) {
+		my $part = $_;
+		if(index($part, "partido") != -1 || index($part, "nombre") != -1) {
+			$part =~ s/"(.*?)"/$1/g;
+			$part =~ s/^\s+|\s+$//g;
+			print ucfirst($part)."\n";
+		}
+	}
+	print "\n-----------------------\n";
+	<STDIN>;
+
+}
+
+exit 0;
